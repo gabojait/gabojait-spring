88cef924bdf8115d18b10d0a023a40d8b86524d0
gs97ahn <gs97ahn@naver.com>
2022-08-10 15:34:26 +0900

    [REFACTOR] 필드 이름 변경

--- src/main/java/com/inuappcenter/gabojaitspring/common/BaseTimeEntity.java
+++ src/main/java/com/inuappcenter/gabojaitspring/common/BaseTimeEntity.java
@@ -2,11 +2,13 @@ package com.inuappcenter.gabojaitspring.common;
 
 import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class BaseTimeEntity extends BaseEntity{
     @LastModifiedDate
+    @Field(name = "modified_date")
     private LocalDateTime modifiedDate;
 }
