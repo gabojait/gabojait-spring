12fd87774d7912f7df7cc9bf9b088b5044095c2d
gs97ahn <gs97ahn@naver.com>
2022-08-13 05:49:06 +0900

    [FEAT] monogodb 관련 annotation 추가

--- src/main/java/com/inuappcenter/gabojaitspring/Application.java
+++ src/main/java/com/inuappcenter/gabojaitspring/Application.java
@@ -1,9 +1,16 @@
 package com.inuappcenter.gabojaitspring;
 
+import com.inuappcenter.gabojaitspring.user.repository.ContactRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = {ContactRepository.class})
+@ComponentScan("com.inuappcenter.*")
+@EnableMongoAuditing
 public class Application {
 
 	public static void main(String[] args) {
