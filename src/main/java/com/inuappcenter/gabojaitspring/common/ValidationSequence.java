13b26dc72adc7feca5064d82bddc7e188ac5e1c2
gs97ahn <gs97ahn@naver.com>
2022-08-20 19:07:57 +0900

    [FEAT] Validation Seqence 구현

---
+++ src/main/java/com/inuappcenter/gabojaitspring/common/ValidationSequence.java
@@ -0,0 +1,9 @@
+package com.inuappcenter.gabojaitspring.common;
+
+public class ValidationSequence {
+    public interface NotBlank {}
+    public interface NotNull {}
+    public interface Email {}
+    public interface Size {}
+    public interface Pattern {}
+}
