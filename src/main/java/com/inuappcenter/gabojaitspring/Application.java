32577c78f16f352e97dc34c9b4576eff11efaa6f
gs97ahn <gs97ahn@naver.com>
2022-08-20 19:32:15 +0900

    [FEAT] 패스워드 인코더

--- src/main/java/com/inuappcenter/gabojaitspring/Application.java
+++ src/main/java/com/inuappcenter/gabojaitspring/Application.java
@@ -1,14 +1,17 @@
 package com.inuappcenter.gabojaitspring;
 
 import com.inuappcenter.gabojaitspring.user.repository.ContactRepository;
+import com.inuappcenter.gabojaitspring.user.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = {ContactRepository.class})
+@EnableMongoRepositories(basePackageClasses = {ContactRepository.class, UserRepository.class})
 @ComponentScan("com.inuappcenter.*")
 @EnableMongoAuditing
 public class Application {
@@ -17,4 +20,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
