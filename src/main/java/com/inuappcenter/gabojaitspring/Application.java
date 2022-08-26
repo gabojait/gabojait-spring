b4d346e7bafb8b9cd80399536739cd978d48fea1
gs97ahn <gs97ahn@naver.com>
2022-08-27 02:16:00 +0900

    [FEAT] BCryptPasswordEncoder를 Bean에 등록하는 구현

--- src/main/java/com/inuappcenter/gabojaitspring/Application.java
+++ src/main/java/com/inuappcenter/gabojaitspring/Application.java
@@ -4,15 +4,11 @@ import com.inuappcenter.gabojaitspring.user.repository.ContactRepository;
 import com.inuappcenter.gabojaitspring.user.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = {ContactRepository.class, UserRepository.class})
-@ComponentScan("com.inuappcenter.*")
 @EnableMongoAuditing
 public class Application {
 
@@ -20,8 +16,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
--- src/main/java/com/inuappcenter/gabojaitspring/auth/PasswordEncoder.java
+++ src/main/java/com/inuappcenter/gabojaitspring/auth/PasswordEncoder.java
@@ -1,2 +1,19 @@
-package com.inuappcenter.gabojaitspring.auth;public class PasswordEncoder {
+package com.inuappcenter.gabojaitspring.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+
+    /**
+     * BCryptPasswordEncoder |
+     * BCryptPasswordEncoder를 Bean에 등록시킨다.
+     * @return
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
