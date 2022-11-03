0623ef49b20a51a3044bd3e4df676410a5619d8b
gs97ahn <gs97ahn@naver.com>
2022-11-03 23:16:21 +0900

    [REFACTOR] 불필요한 setVerificationCode 제거

--- src/main/java/com/inuappcenter/gabojaitspring/user/domain/Contact.java
+++ src/main/java/com/inuappcenter/gabojaitspring/user/domain/Contact.java
@@ -5,16 +5,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Getter
 @Document(collection = "contact")
@@ -40,10 +33,6 @@ public class Contact extends BaseTimeEntity {
         this.verificationCode = verificationCode;
         this.isVerified = false;
         this.isRegistered = false;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
     }
 
     public void setIsVerified(Boolean isVerified) {
