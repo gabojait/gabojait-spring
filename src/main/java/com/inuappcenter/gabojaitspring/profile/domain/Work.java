8ef330b2538618b750196f077ef9db22c659cbb5
gs97ahn <gs97ahn@naver.com>
2022-11-05 02:14:06 +0900

    [FEAT] 경력 도메인 추가

--- src/main/java/com/inuappcenter/gabojaitspring/profile/domain/Work.java
+++ src/main/java/com/inuappcenter/gabojaitspring/profile/domain/Work.java
@@ -1,30 +1,71 @@
 package com.inuappcenter.gabojaitspring.profile.domain;
 
+import com.inuappcenter.gabojaitspring.common.BaseTimeEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.time.LocalDate;
+
 @Getter
 @Document(collection = "work")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Work {
+public class Work extends BaseTimeEntity {
 
     @Field(name = "corporation_name")
     private String corporationName;
 
+    private String about;
+
     @Field(name = "started_date")
-    private String startedDate;
+    private LocalDate startedDate;
 
     @Field(name = "ended_date")
-    private String endedDate;
+    private LocalDate endedDate;
 
     @Field(name = "is_current")
     private Boolean isCurrent;
 
+    @Field(name = "user_id")
+    private String userId;
+
     @Field(name = "is_deleted")
     private Boolean isDeleted;
 
+    @Builder(builderClassName = "ByWorkBuilder", builderMethodName = "ByWorkBuilder")
+    public Work(String corporationName,
+                String about,
+                LocalDate startedDate,
+                LocalDate endedDate,
+                Boolean isCurrent) {
+        this.corporationName = corporationName;
+        this.about = about;
+        this.startedDate = startedDate;
+        this.endedDate = endedDate;
+        this.isCurrent = isCurrent;
+        this.isDeleted = false;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public void update(String corporationName,
+                       String about,
+                       LocalDate startedDate,
+                       LocalDate endedDate,
+                       Boolean isCurrent) {
+        this.corporationName = corporationName;
+        this.about = about;
+        this.startedDate = startedDate;
+        this.endedDate = endedDate;
+        this.isCurrent = isCurrent;
+    }
 }
