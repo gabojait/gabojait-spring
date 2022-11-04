670e3f077eb39a43628ea47c860828db92b9079a
gs97ahn <gs97ahn@naver.com>
2022-11-05 02:13:49 +0900

    [FEAT] 스킬 도메인 추가

--- src/main/java/com/inuappcenter/gabojaitspring/profile/domain/Skill.java
+++ src/main/java/com/inuappcenter/gabojaitspring/profile/domain/Skill.java
@@ -1,10 +1,13 @@
 package com.inuappcenter.gabojaitspring.profile.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDate;
 
 @Getter
 @Document(collection = "skill")
@@ -14,8 +17,60 @@ public class Skill {
     @Field(name = "skill_name")
     private String skillName;
 
-    private Integer level;
+    private Byte level;
+
+    @Field(name = "is_experienced")
+    private Boolean isExperienced;
+
+    @Field(name = "started_date")
+    private LocalDate startedDate;
+
+    @Field(name = "ended_date")
+    private LocalDate endedDate;
+
+    @Field(name = "is_current")
+    private Boolean isCurrent;
+
+    @Field(name = "user_id")
+    private String userId;
 
     @Field(name = "is_deleted")
     private Boolean isDeleted;
+
+    @Builder(builderClassName = "BySkillBuilder", builderMethodName = "BySkillBuilder")
+    public Skill(String skillName,
+                 Byte level,
+                 Boolean isExperienced,
+                 LocalDate startedDate,
+                 LocalDate endedDate,
+                 Boolean isCurrent) {
+        this.skillName = skillName;
+        this.level = level;
+        this.isExperienced = isExperienced;
+        this.startedDate = startedDate;
+        this.endedDate = endedDate;
+        this.isCurrent = isCurrent;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public void update(String skillName,
+                       Byte level,
+                       Boolean isExperienced,
+                       LocalDate startedDate,
+                       LocalDate endedDate,
+                       Boolean isCurrent) {
+        this.skillName = skillName;
+        this.level = level;
+        this.isExperienced = isExperienced;
+        this.startedDate = startedDate;
+        this.endedDate = endedDate;
+        this.isCurrent = isCurrent;
+    }
 }
