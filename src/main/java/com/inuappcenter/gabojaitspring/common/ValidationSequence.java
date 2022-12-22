486ccda0c1d7af9f8cd917a27cca6a34af3241b4
gs97ahn <gs97ahn@naver.com>
2022-12-22 21:14:35 +0900

    [REFACTOR] abstract 클래스로 변경

--- src/main/java/com/inuappcenter/gabojaitspring/common/ValidationSequence.java
+++ src/main/java/com/inuappcenter/gabojaitspring/common/ValidationSequence.java
@@ -1,6 +1,6 @@
 package com.inuappcenter.gabojaitspring.common;
 
-public class ValidationSequence {
+public abstract class ValidationSequence {
     public interface NotBlank {}
     public interface NotNull {}
     public interface Email {}
