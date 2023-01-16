306920f2d230c592c1e84631f85946dd508e49de
gs97ahn <gs97ahn@naver.com>
2023-01-16 21:43:26 +0900

    [REFACTOR] 평점 user domain에서 profile domain으로 수정

--- src/main/java/com/inuappcenter/gabojaitspring/user/dto/UserDefaultResponseDto.java
+++ src/main/java/com/inuappcenter/gabojaitspring/user/dto/UserDefaultResponseDto.java
@@ -32,21 +32,18 @@ public class UserDefaultResponseDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate birthdate;
 
-    @ApiModelProperty(position = 7, required = true, value = "평점")
-    private Float rating;
-
-    @ApiModelProperty(position = 8, required = true, value = "연락처")
+    @ApiModelProperty(position = 7, required = true, value = "연락처")
     private ContactDefaultResponseDto contact;
 
-    @ApiModelProperty(position = 9, required = true, value = "생성일")
+    @ApiModelProperty(position = 8, required = true, value = "생성일")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdDate;
 
-    @ApiModelProperty(position = 10, required = true, value = "수정일")
+    @ApiModelProperty(position = 9, required = true, value = "수정일")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedDate;
 
-    @ApiModelProperty(position = 11, required = true, value = "스키마버전")
+    @ApiModelProperty(position = 10, required = true, value = "스키마버전")
     private String schemaVersion;
 
     public UserDefaultResponseDto(User user) {
@@ -56,7 +53,6 @@ public class UserDefaultResponseDto {
         this.nickname = user.getNickname();
         this.gender = user.getGender();
         this.birthdate = user.getBirthdate();
-        this.rating = user.getRating();
         this.contact = new ContactDefaultResponseDto(user.getContact());
         this.createdDate = user.getCreatedDate();
         this.modifiedDate = user.getModifiedDate();
