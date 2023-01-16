9f1fe5f6face3c2c161d06e5bb6ce977836616b7
gs97ahn <gs97ahn@naver.com>
2023-01-17 03:40:30 +0900

    [FEAT] NotEmpty 인터페이스 추가

--- src/main/java/com/inuappcenter/gabojaitspring/common/ValidationSequence.java
+++ src/main/java/com/inuappcenter/gabojaitspring/common/ValidationSequence.java
@@ -4,6 +4,7 @@ public abstract class ValidationSequence {
 
     public interface NotBlank {}
     public interface NotNull {}
+    public interface NotEmpty {}
     public interface Email {}
     public interface Size {}
     public interface Pattern {}
