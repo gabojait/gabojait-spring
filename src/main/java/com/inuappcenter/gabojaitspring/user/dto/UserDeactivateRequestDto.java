9ed69b5e475895657d903cc5fbb2a23e078d1018
gs97ahn <gs97ahn@naver.com>
2023-01-18 13:47:10 +0900

    [FIX] password 예시 수정

--- src/main/java/com/inuappcenter/gabojaitspring/user/dto/UserDeactivateRequestDto.java
+++ src/main/java/com/inuappcenter/gabojaitspring/user/dto/UserDeactivateRequestDto.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotBlank;
 @ApiModel(value = "User 탈퇴 요청")
 public class UserDeactivateRequestDto {
 
-    @ApiModelProperty(position = 1, required = true, value = "비밀번호", example = "password")
+    @ApiModelProperty(position = 1, required = true, value = "비밀번호", example = "password1!")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
