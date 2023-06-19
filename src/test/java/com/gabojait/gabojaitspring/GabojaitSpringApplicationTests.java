bd1dcedd693e17c0a7235977350ca44537d603b8
gs97ahn <gs97ahn@naver.com>
2023-06-19 20:14:59 +0900

    [TEST] environment variable add

--- src/test/java/com/gabojait/gabojaitspring/GabojaitSpringApplicationTests.java
+++ src/test/java/com/gabojait/gabojaitspring/GabojaitSpringApplicationTests.java
@@ -1,7 +1,11 @@
 package com.gabojait.gabojaitspring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 class GabojaitSpringApplicationTests {
 
     @Test
