e28205c75a0455e7fa63bf48ca9df74d989834f1
gs97ahn <gs97ahn@naver.com>
2023-06-19 20:27:35 +0900

    [TEST] assertNotNull for applicationContext add

--- src/test/java/com/gabojait/gabojaitspring/GabojaitSpringApplicationTests.java
+++ src/test/java/com/gabojait/gabojaitspring/GabojaitSpringApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.yml")
 class GabojaitSpringApplicationTests {
@@ -15,6 +17,7 @@ class GabojaitSpringApplicationTests {
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
