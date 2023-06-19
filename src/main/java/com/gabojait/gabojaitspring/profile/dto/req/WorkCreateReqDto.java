5f379a15d5b7c2d1e9c48e7332895e3b5d510b66
gs97ahn <gs97ahn@naver.com>
2023-06-19 18:29:46 +0900

    [STYLE] modify field order

--- src/main/java/com/gabojait/gabojaitspring/profile/dto/req/WorkCreateReqDto.java
+++ src/main/java/com/gabojait/gabojaitspring/profile/dto/req/WorkCreateReqDto.java
@@ -31,23 +31,23 @@ public class WorkCreateReqDto {
     @Size(min = 1, max = 20, message = "기관명은 1~20자만 가능합니다.", groups = ValidationSequence.Size.class)
     private String corporationName;
 
-    @ApiModelProperty(position = 2, required = true, value = "시작일", notes = "string", example = "2000-01-01")
+    @ApiModelProperty(position = 2, value = "경력 설명", example = "가보자잇에서 백엔드 개발")
+    @Size(max = 100, message = "경력 설명은 0~100자만 가능합니다.", groups = ValidationSequence.Size.class)
+    private String workDescription;
+
+    @ApiModelProperty(position = 3, required = true, value = "시작일", notes = "string", example = "2000-01-01")
     @NotNull(message = "시작일을 입력해 주세요.", groups = ValidationSequence.Blank.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate startedAt;
 
-    @ApiModelProperty(position = 3, value = "종료일", notes = "string", example = "2000-01-02")
+    @ApiModelProperty(position = 4, value = "종료일", notes = "string", example = "2000-01-02")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate endedAt;
 
-    @ApiModelProperty(position = 4, required = true, value = "현재 여부", example = "true",
+    @ApiModelProperty(position = 5, required = true, value = "현재 여부", example = "true",
             allowableValues = "true, false")
     @NotNull(message = "현재 여부를 입력해 주세요.", groups = ValidationSequence.Blank.class)
     private Boolean isCurrent;
-
-    @ApiModelProperty(position = 5, value = "경력 설명", example = "가보자잇에서 백엔드 개발")
-    @Size(max = 100, message = "경력 설명은 0~100자만 가능합니다.", groups = ValidationSequence.Size.class)
-    private String workDescription;
 
     public Work toEntity(User user) {
         return Work.builder()
