745a5116994523f774d152753f4c6f00ce818ff2
gs97ahn <gs97ahn@naver.com>
2023-06-19 20:43:51 +0900

    [CHORE] mysql setup for test

--- src/test/java/com/gabojait/gabojaitspring/GabojaitSpringApplicationTests.java
+++ src/test/java/com/gabojait/gabojaitspring/GabojaitSpringApplicationTests.java
@@ -1,23 +1,15 @@
 package com.gabojait.gabojaitspring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.yml")
 class GabojaitSpringApplicationTests {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Test
     void contextLoads() {
-        assertNotNull(applicationContext);
     }
 
 }
