af1ead82ee41b6be3d3d563334697933ee1cf873
gs97ahn <gs97ahn@naver.com>
2023-06-24 08:43:49 +0900

    [REFACTOR] change url for docs

--- src/main/java/com/gabojait/gabojaitspring/develop/controller/DocumentController.java
+++ src/main/java/com/gabojait/gabojaitspring/develop/controller/DocumentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/api/v1/docs")
+@RequestMapping("/docs")
 public class DocumentController {
 
     @GetMapping("/privacy")
