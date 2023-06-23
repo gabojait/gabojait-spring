999b982d68a77e94178d7dc299fb4d8f17fe71a1
gs97ahn <gs97ahn@naver.com>
2023-06-24 08:39:49 +0900

    [FEAT] privacy and service page add

---
+++ src/main/java/com/gabojait/gabojaitspring/develop/controller/DocumentController.java
@@ -0,0 +1,25 @@
+package com.gabojait.gabojaitspring.develop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/api/v1/docs")
+public class DocumentController {
+
+    @GetMapping("/privacy")
+    public ModelAndView privacyPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("privacy.html");
+        return modelAndView;
+    }
+
+    @GetMapping("/service")
+    public ModelAndView servicePage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("service.html");
+        return modelAndView;
+    }
+}
