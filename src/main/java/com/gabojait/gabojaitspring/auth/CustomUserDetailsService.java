aa76fbe2e587080fcededa21ea6bcedea574e3a5
gs97ahn <gs97ahn@naver.com>
2023-06-26 22:00:32 +0900

    [REFACTOR] add transactional annot

--- src/main/java/com/gabojait/gabojaitspring/auth/CustomUserDetailsService.java
+++ src/main/java/com/gabojait/gabojaitspring/auth/CustomUserDetailsService.java
@@ -8,11 +8,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.gabojait.gabojaitspring.common.code.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
