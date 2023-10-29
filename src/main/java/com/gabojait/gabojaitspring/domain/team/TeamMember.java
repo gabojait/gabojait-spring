eb490f8c751edbb143cd216ef73b73e9e82a2fe2
gs97ahn <gs97ahn@naver.com>
2023-10-29 22:40:18 +0900

    [REFACTOR] user nullable

--- src/main/java/com/gabojait/gabojaitspring/domain/team/TeamMember.java
+++ src/main/java/com/gabojait/gabojaitspring/domain/team/TeamMember.java
@@ -22,7 +22,7 @@ public class TeamMember extends BasePermanentEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
