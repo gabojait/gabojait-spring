88ce68ef8a89d4d91abc9da123ed1b33f3af3857
gs97ahn <gs97ahn@naver.com>
2023-11-11 02:52:51 +0900

    [TEST] refactor notification related tests

--- src/test/java/com/gabojait/gabojaitspring/domain/notification/FcmTest.java
+++ src/test/java/com/gabojait/gabojaitspring/domain/notification/FcmTest.java
@@ -5,10 +5,15 @@ import com.gabojait.gabojaitspring.domain.user.Gender;
 import com.gabojait.gabojaitspring.domain.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FcmTest {
@@ -28,14 +33,78 @@ class FcmTest {
         assertEquals(fcm.getFcmToken(), fcmToken);
     }
 
-    private Fcm createFcm(String fcmToken, User user) {
+    private static Stream<Arguments> providerEquals() {
+        User user = createDefaultUser("tester@gabojait.com", "000000", "tester", "password1!", "테스터", Gender.F,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+        String fcmToken = "fcm-token";
+
+        Fcm fcm = createFcm(fcmToken, user);
+
+        User user1 = createDefaultUser("tester1@gabojait.com", "000000", "tester1", "password1!", "테스터일", Gender.M,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+        Fcm userFcm1 = createFcm(fcmToken, user1);
+        User user2 = createDefaultUser("tester2@gabojait.com", "000000", "tester2", "password1!", "테스터이", Gender.M,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+        Fcm userFcm2 = createFcm(fcmToken, user2);
+
+        return Stream.of(
+                Arguments.of(fcm, fcm, true),
+                Arguments.of(fcm, new Object(), false),
+                Arguments.of(
+                        createFcm("fcm-token1", user),
+                        createFcm("fcm-token2", user),
+                        false
+                ),
+                Arguments.of(userFcm1, userFcm2, false)
+        );
+    }
+
+    @ParameterizedTest(name = "[{index}] FCM 객체를 비교한다.")
+    @MethodSource("providerEquals")
+    @DisplayName("FCM 객체를 비교한다.")
+    void givenProvider_whenEquals_thenReturn(Fcm fcm, Object object, boolean result) {
+        // when & then
+        assertThat(fcm.equals(object)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> providerHashCode() {
+        User user = createDefaultUser("tester@gabojait.com", "000000", "tester", "password1!", "테스터", Gender.F,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+
+        return Stream.of(
+                Arguments.of(
+                        createFcm("fcm-token", user),
+                        createFcm("fcm-token", user),
+                        true
+                ),
+                Arguments.of(
+                        createFcm("fcm-token1", user),
+                        createFcm("fcm-token2", user),
+                        false
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "[{index}] FCM 해시코드를 비교한다.")
+    @MethodSource("providerHashCode")
+    @DisplayName("FCM 해시코드를 비교한다.")
+    void givenProvider_whenHashCode_thenReturn(Fcm fcm1, Fcm fcm2, boolean result) {
+        // when
+        int hashCode1 = fcm1.hashCode();
+        int hashCode2 = fcm2.hashCode();
+
+        // then
+        assertThat(hashCode1 == hashCode2).isEqualTo(result);
+    }
+
+    private static Fcm createFcm(String fcmToken, User user) {
         return Fcm.builder()
                 .fcmToken(fcmToken)
                 .user(user)
                 .build();
     }
 
-    private User createDefaultUser(String email,
+    private static User createDefaultUser(String email,
                                    String verificationCode,
                                    String username,
                                    String password,
--- src/test/java/com/gabojait/gabojaitspring/domain/notification/NotificationTest.java
+++ src/test/java/com/gabojait/gabojaitspring/domain/notification/NotificationTest.java
@@ -5,9 +5,13 @@ import com.gabojait.gabojaitspring.domain.user.Gender;
 import com.gabojait.gabojaitspring.domain.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,7 +52,89 @@ class NotificationTest {
         assertTrue(notification.getIsRead());
     }
 
-    private Notification createNotification(User user, NotificationType notificationType, String title, String body) {
+    private static Stream<Arguments> providerEquals() {
+        User user = createDefaultUser("tester@gabojait.com", "000000", "tester", "password1!", "테스터", Gender.F,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+        Notification notification = createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다.");
+
+        User user1 = createDefaultUser("tester1@gabojait.com", "000000", "tester1", "password1!", "테스터일", Gender.F,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+        Notification userNotification1 = createNotification(user1, NotificationType.NONE, "알림 제목", "알림이 왔습니다.");
+        User user2 = createDefaultUser("tester2@gabojait.com", "000000", "tester2", "password1!", "테스터이", Gender.F,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+        Notification userNotification2 = createNotification(user2, NotificationType.NONE, "알림 제목", "알림이 왔습니다.");
+
+        Notification isReadNotification1 = createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다.");
+        Notification isReadNotification2 = createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다.");
+        isReadNotification2.read();
+
+        return Stream.of(
+                Arguments.of(notification, notification, true),
+                Arguments.of(notification, new Object(), false),
+                Arguments.of(
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다."),
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다."),
+                        true
+                ),
+                Arguments.of(
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다."),
+                        createNotification(user, NotificationType.USER_OFFER, "알림 제목", "알림이 왔습니다."),
+                        false
+                ),
+                Arguments.of(
+                        createNotification(user, NotificationType.NONE, "알림 제목1", "알림이 왔습니다."),
+                        createNotification(user, NotificationType.NONE, "알림 제목2", "알림이 왔습니다."),
+                        false
+                ),
+                Arguments.of(
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다.1"),
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다.2"),
+                        false
+                ),
+                Arguments.of(userNotification1, userNotification2, false),
+                Arguments.of(isReadNotification1, isReadNotification2, false)
+        );
+    }
+
+    @ParameterizedTest(name = "[{index}] 알림 객체를 비교한다.")
+    @MethodSource("providerEquals")
+    @DisplayName("알림 객체를 비교한다.")
+    void givenProvider_whenEquals_thenReturn(Notification notification, Object object, boolean result) {
+        // when & then
+        assertThat(notification.equals(object)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> providerHashCode() {
+        User user = createDefaultUser("tester@gabojait.com", "000000", "tester", "password1!", "테스터", Gender.F,
+                LocalDate.of(1997, 2, 11), LocalDateTime.now());
+
+        return Stream.of(
+                Arguments.of(
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다."),
+                        createNotification(user, NotificationType.NONE, "알림 제목", "알림이 왔습니다."),
+                        true
+                ),
+                Arguments.of(
+                        createNotification(user, NotificationType.NONE, "알림 제목1", "알림이 왔습니다."),
+                        createNotification(user, NotificationType.NONE, "알림 제목2", "알림이 왔습니다."),
+                        false
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "[{index}] 알림 해시코드를 비교한다.")
+    @MethodSource("providerHashCode")
+    @DisplayName("알림 해시코드를 비교한다.")
+    void givenProvider_whenHashCode_thenReturn(Notification notification1, Notification notification2, boolean result) {
+        // when
+        int hashCode1 = notification1.hashCode();
+        int hashCode2 = notification2.hashCode();
+
+        // then
+        assertThat(hashCode1 == hashCode2).isEqualTo(result);
+    }
+
+    private static Notification createNotification(User user, NotificationType notificationType, String title, String body) {
         return Notification.builder()
                 .user(user)
                 .notificationType(notificationType)
@@ -58,14 +144,14 @@ class NotificationTest {
 
     }
 
-    private User createDefaultUser(String email,
-                                   String verificationCode,
-                                   String username,
-                                   String password,
-                                   String nickname,
-                                   Gender gender,
-                                   LocalDate birthdate,
-                                   LocalDateTime lastRequestAt) {
+    private static User createDefaultUser(String email,
+                                          String verificationCode,
+                                          String username,
+                                          String password,
+                                          String nickname,
+                                          Gender gender,
+                                          LocalDate birthdate,
+                                          LocalDateTime lastRequestAt) {
         Contact contact = Contact.builder()
                 .email(email)
                 .verificationCode(verificationCode)
