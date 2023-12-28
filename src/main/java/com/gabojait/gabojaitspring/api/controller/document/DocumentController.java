a307891a3a041179b5d8a95b67ef6cfbf9f123cc
gs97ahn <gs97ahn@naver.com>
2023-12-28 23:29:57 +0900

    [REFACTOR] docs pages remove .html

--- src/main/java/com/gabojait/gabojaitspring/api/controller/document/DocumentController.java
+++ src/main/java/com/gabojait/gabojaitspring/api/controller/document/DocumentController.java
@@ -3,23 +3,18 @@ package com.gabojait.gabojaitspring.api.controller.document;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/docs")
 public class DocumentController {
 
-    @GetMapping("/privacy.html")
-    public ModelAndView privacy() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("privacy");
-        return modelAndView;
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
     }
 
-    @GetMapping("/service.html")
-    public ModelAndView service() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("service");
-        return modelAndView;
+    @GetMapping("/service")
+    public String service() {
+        return "service";
     }
 }
