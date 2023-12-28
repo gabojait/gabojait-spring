8f72f6bc700a8add8647aeb05ff9d487aea731c9
gs97ahn <gs97ahn@naver.com>
2023-12-28 20:42:20 +0900

    [REFACTOR] remove GroupSequence in TeamIsRequestingUpdateRequest

--- src/main/java/com/gabojait/gabojaitspring/api/dto/team/request/TeamIsRecruitingUpdateRequest.java
+++ src/main/java/com/gabojait/gabojaitspring/api/dto/team/request/TeamIsRecruitingUpdateRequest.java
@@ -1,24 +1,21 @@
 package com.gabojait.gabojaitspring.api.dto.team.request;
 
-import com.gabojait.gabojaitspring.api.dto.common.ValidationSequence;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
-import javax.validation.GroupSequence;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@GroupSequence({TeamIsRecruitingUpdateRequest.class, ValidationSequence.Blank.class})
 @ApiModel(value = "팀원 모집 여부 수정 요청")
 public class TeamIsRecruitingUpdateRequest {
 
     @ApiModelProperty(position = 1, required = true, value = "팀원 모집 여부", example = "true",
             allowableValues = "true, false")
-    @NotNull(message = "팀원 모집 여부는 필수 입력입니다.", groups = ValidationSequence.Blank.class)
+    @NotNull(message = "팀원 모집 여부는 필수 입력입니다.")
     private Boolean isRecruiting;
 
     @Builder
