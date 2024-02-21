4a884cae02da707a754281b87ee0e9614e1b2898
gs97ahn <gs97ahn@naver.com>
2024-02-21 23:14:39 +0900

    [FIX] change deep link url

--- src/main/java/com/gabojait/gabojaitspring/domain/notification/DeepLinkType.java
+++ src/main/java/com/gabojait/gabojaitspring/domain/notification/DeepLinkType.java
@@ -9,9 +9,9 @@ public enum DeepLinkType {
 
     HOME_PAGE("gabojait://MainBottomTabNavigation/Home/GroupList", "홈페이지"),
     TEAM_PAGE("gabojait://MainBottomTabNavigation/Team", "팀 페이지"),
-    REVIEW_PAGE("gabojait://MainNavigation/ApplyStatus", "리뷰 작성 페이지"),
+    REVIEW_PAGE("gabojait://MainNavigation/TeamHistory", "리뷰 작성 페이지"),
     USER_OFFER_RECEIVE_PAGE("gabojait://MainNavigation/OfferFromTeam", "회원 제안 페이지"),
-    TEAM_OFFER_RECEIVE_PAGE("gabojait://MainNavigation/TeamHistory", "팀 제안 페이지")
+    TEAM_OFFER_RECEIVE_PAGE("gabojait://MainNavigation/ApplyStatus", "팀 제안 페이지")
     ;
 
     private final String url;
